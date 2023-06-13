71c3f28f46003f4ac08b7a620faa39b3a1e4141f
MOUAD ASSILA (BAGA-37) <94559590+MOUADASSILA@users.noreply.github.com>
2023-06-13 19:47:01 +0300

    Update projet.js

--- projet.js
+++ projet.js
@@ -5,21 +5,17 @@ const arrowBtns = document.querySelectorAll(".wrapper i");
 const carouselChildren = [...carousel.Children];
 
 let isDragging = false , isAutoPlay = true , startX, startScrollLeft, timeoutId;
-//get the number of cards that can fit in the carousel at once
 let cardPerView = math.round(carousel.offsetWidth / firstCardWidth);
-//insert copies of the last fiew card to beginning of caousel for imfinite scrolling
+
 carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
     carousel.insertAdjacentHTML("afterbegin",card.outerHTML);
 });
-//insert copies of the last fiew card to end of caousel for imfinite scrolling 
 carouselChildrens.slice(0, cardPerView).reverse().forEach(card => {
     carousel.insertAdjacentHTML("beforeend",card.outerHTML);
 });
-//scrool the carousel at appropiate position to hide first few duplicate cards on firefox
 carousel.classList.add("no-transition");
 carousel.scrollLeft = carousel.offsetWidth;
 carousel.classList.remove("no-transition");
-//add event listeners for the arrow button to scroll the carousel left and right
 arrowBtns.forEach ( btn => {
     btn.addEventListener("click". () => {
         carousel.scrollLeft += btn.id =="left" ? -firstCadrWidth : firstCadrWidth ;
@@ -28,14 +24,12 @@ arrowBtns.forEach ( btn => {
 const dragStart = (e) => {
     isDragging = true ;
     carousel.classList.add("dragging");
-    //records the initial cursor and scroll position of thecarousel
     startX = e.pageX;
     startScrollLeft = carousel.scrollLeft;
 }
 const dragging = (e) => {
     isDragging = true;
     carousel.classList.add("dragging");
-    //records the initial cursor and scroll position of the carousel
     startX = e.pageX;
     startScrollLeft = carousel.scrollLeft;
 }
